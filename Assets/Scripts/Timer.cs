e3751f0d49074fe3bb58dd15d9f9aff2bae4fb97
Marjut Ala-Ketola <Marjut2.Ala-Ketola@live.uwe.ac.uk>
2023-01-18 14:00:32 +0000

    Finished Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Timer : MonoBehaviour
@@ -9,22 +10,44 @@ public class Timer : MonoBehaviour
     public bool timerOn = false;
 
     public TMP_Text timerTxt;
+    public TMP_Text sliderTimerText;
+
+    public Slider slider;
+    public TMP_Text timeText;
+    public float gametime;
+    float time;
+
+    private void Start()
+    {
+        slider.onValueChanged.AddListener((v) =>
+        {
+            float minutes = Mathf.FloorToInt(v / 60);
+            float seconds = Mathf.FloorToInt(v % 60);
+
+            sliderTimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+
+            gametime = v;
+        });
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(timerOn)
-        {
-            if(timeLeft > 0)
+        if (timerOn)
+        { 
+            if (timeLeft > 0)
             {
                 timeLeft -= Time.deltaTime;
                 updateTimer(timeLeft);
+                time = gametime - Time.time;
+                updateSliderTimer(time);           
             }
             else
             {
                 Debug.Log("Time is up");
                 timeLeft = 0;
                 timerOn = false;
+                slider.value = time;
             }
         }
     }
@@ -39,6 +62,14 @@ public class Timer : MonoBehaviour
         timerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
     }
 
+    private void updateSliderTimer(float currentTime)
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        sliderTimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+
     public void PauseTimer()
     {
         timerOn = !timerOn;
@@ -46,7 +77,7 @@ public class Timer : MonoBehaviour
 
     public void StartTimer()
     {
-        timeLeft = 600;
+        timeLeft = slider.value;
         timerOn = true;
     }
 
